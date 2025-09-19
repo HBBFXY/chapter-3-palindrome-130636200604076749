fe1647d28d7e5cfc5e02df17000f63e7bdc6fc71
130636200604076749 <jiafeimao1188@qq.com>
2025-09-19 11:21:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-def is_valid_input(input_str):
-    return len(input_str) == 5 and input_str.isdigit()
-
-def check_palindrome(number_str):
-    return number_str == number_str[::-1]
-if __name__ == "__main__":
-    user_input = input("请输入一个5位数字（例如：12321）：")
-    if not is_valid_input(user_input):
-        print("错误提示：请输入一个5位纯数字")
+num = input("请输入一个5位数字：")
+if len(num) != 5 or not num.isdigit():
+    print("错误提示：请输入一个5位纯数字")
+else:
+    if num == num[::-1]:
+        print("是回文数")
     else:
-        result = "是回文数" if check_palindrome(user_input) else "不是回文数"
-        print(result)
+        print("不是回文数")
