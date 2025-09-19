6d2483fbc293e3e9a19156795ab453c44f93d9bf
130636200604076749 <jiafeimao1188@qq.com>
2025-09-19 10:57:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ if len(num) != 5 or not num.isdigit():
     print("错误提示：请输入一个5位纯数字")
 else:
     reversed_num = ""
-    # 手动拼接反转字符串
     for char in num:
         reversed_num = char + reversed_num
-    # 判断是否回文
     print("是回文数" if num == reversed_num else "不是回文数")
