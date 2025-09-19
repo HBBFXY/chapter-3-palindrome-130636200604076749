46c5ac1829a75176da73fab89dae0f131e49f11f
130636200604076749 <jiafeimao1188@qq.com>
2025-09-19 11:04:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-# 从键盘获取输入
 num_str = input("请输入一个5位数字：")
-
-# 校验输入是否为5位且纯数字
 if len(num_str) != 5 or not num_str.isdigit():
     print("错误提示：请输入一个5位纯数字")
 else:
-    # 利用字符串切片反转并判断回文
     if num_str == num_str[::-1]:
         print("是回文数")
     else:
