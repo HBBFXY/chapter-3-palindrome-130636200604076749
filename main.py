fa8145d25169737f564145e431262d11ad875b59
130636200604076749 <jiafeimao1188@qq.com>
2025-09-19 10:50:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-num_str = input("请输入一个5位数字:")
-if len(num_str) != 5 or not num_str.isdigit():
+num = input("请输入一个5位数字：")
+if len(num) != 5 or not num.isdigit():
     print("错误提示：请输入一个5位纯数字")
 else:
-    is_palindrome = True
-    for i in range(len(num_str) // 2):
-        if num_str[i] != num_str[len(num_str) - 1 - i]:
-            is_palindrome = False
-            break
-    print("是回文数" if is_palindrome else "不是回文数")
+    reversed_num = ""
+    for char in num:
+        reversed_num = char + reversed_num
+    print("是回文数" if num == reversed_num else "不是回文数")
