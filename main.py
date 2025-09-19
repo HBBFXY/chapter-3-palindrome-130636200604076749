d7afb85f2c3009f201639cbc3e34ef8dccb51625
130636200604076749 <jiafeimao1188@qq.com>
2025-09-19 10:42:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-num = input("请输入一个5位数字:")
-if len(num) != 5 or not num.isdigit():
+input_num = input("请输入一个5位数字:")
+if len(input_num) != 5 or not input_num.isdecimal():
     print("错误提示：请输入一个5位纯数字")
 else:
     reversed_num = input_num[::-1]
