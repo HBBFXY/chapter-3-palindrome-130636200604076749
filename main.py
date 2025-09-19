1aa80699550154189def50fd330639b930f0de7f
130636200604076749 <jiafeimao1188@qq.com>
2025-09-19 10:43:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ if len(input_num) != 5 or not input_num.isdecimal():
     print("错误提示：请输入一个5位纯数字")
 else:
     reversed_num = input_num[::-1]
-    if num == reversed_num:
+    if input_num == reversed_num:
         print("是回文数")
     else:
         print("不是回文数")
